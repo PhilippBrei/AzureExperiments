396d637daf7fdbaa893a2e04111cd6516a6f91ea
Philipp Klose <Philipp.Klose@outlook.com>
2020-04-11 21:04:51 +0200

    Some linting

--- aab/test/logger.py
+++ aab/test/logger.py
@@ -1,10 +1,11 @@
+"""Für remote Testauführung"""
+
 import logging
 
-from enum import Enum
 
 from azure_storage_logging.handlers import TableStorageHandler
 from aab.konfiguration.gwi_konfiguration import IntegrationsFluss
-from aab.logging.AABFilter import IntegrationsLauf, AABFachlicherFilter
+from aab.logging.aab_filter import IntegrationsLauf, AABFachlicherFilter
 from datetime import datetime
 
 
@@ -13,19 +14,19 @@ class AzureKonfiguration(object):
     StorageAccountKey = '-'
 
 
-def getaablogger(_name=None):
-    azureKonfiguration = AzureKonfiguration()
-    tableStorageHandler = TableStorageHandler(
-        account_name=azureKonfiguration.StorageAccountName,
-        account_key=azureKonfiguration.StorageAccountKey,
+def get_aab_logger(_name=None):
+    azure_konfiguration = AzureKonfiguration()
+    table_storage_handler = TableStorageHandler(
+        account_name=azure_konfiguration.StorageAccountName,
+        account_key=azure_konfiguration.StorageAccountKey,
         extra_properties=['%(hostname)s', '%(levelname)s', '%(integrations_fluss)s', '%(integrationslauf_stichtag)s', '%(integrationslauf_laufnummer)s'])
     logger = logging.getLogger(_name)
-    logger.addHandler(tableStorageHandler)
+    logger.addHandler(table_storage_handler)
     logger.addFilter(AABFachlicherFilter())
     return logger
 
 
 if __name__ == "__main__":
-    log = getaablogger("Logger")
+    log = get_aab_logger("Logger")
     log.warning("Hallo", IntegrationsFluss.A360_ENTLADE_DARLEHEN,
                 IntegrationsLauf(datetime(2020, 4, 1), 1))
